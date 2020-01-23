478009500dc75a7f7ecc32e3682e2737057c400b
Fzeak <ashleykoh24@gmail.com>
2020-01-23 11:00:58 +0800

    added lamboghini model

--- js/index.js
+++ js/index.js
@@ -1,24 +1,35 @@
 var scene = new THREE.Scene();
+scene.background = new THREE.Color( 0xffffff );
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth / 2, window.innerHeight / 2 );
+hlight = new THREE.AmbientLight(0x404040, 100);
+scene.add(hlight);
+
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize( window.innerWidth / 1.5, window.innerHeight / 1.5 );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// var cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
 
-camera.position.z = 5;
+let loader = new THREE.GLTFLoader();
+loader.load('Lamborghini_Aventador.gltf', function(gltf){
+  scene.add(gltf.scene);
+  renderer.render(scene, camera);
+})
 
-var animate = function () {
-  requestAnimationFrame( animate );
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+// camera.position.z = 5;
 
-  renderer.render( scene, camera );
-};
+// var animate = function () {
+//   requestAnimationFrame( animate );
 
-animate();
+//   cube.rotation.x += 0.01;
+//   cube.rotation.y += 0.01;
+
+//   renderer.render( scene, camera );
+// };
+
+// animate();
