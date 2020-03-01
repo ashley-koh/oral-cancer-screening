6e629704e116b366f79b0893a51fcd86b80bc7c9
ashley-koh <36570313+ashley-koh@users.noreply.github.com>
2020-03-01 12:26:18 +0800

    converted to use gltf with mouth object

--- js/index.js
+++ js/index.js
@@ -1,100 +1,124 @@
-const mtlFile = 'model/Lamborghini_Aventador.mtl';
-const objFile = 'model/Lamborghini_Aventador.obj';
-
 const labels = [
-  {
-    linkName: 'left-door',
-    text: 'Left Door',
-    position: { x: 110, y: 50, z: 0 }
-  },{
-    linkName: 'right-door',
-    text: 'Right Door',
-    position: { x: -110, y: 50, z: 0 }
-  },{
-    linkName: 'bonnet',
-    text: 'Bonnet',
-    position: { x: 0, y: 70, z: 200 }
-  },{
-    linkName: 'boot',
-    text: 'Boot',
-    position: { x: 0, y: 70, z: -250 }
-  }
 ];
 
 
 let scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xffffff );
 
-let camera = new THREE.PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 1, 10000 );
-camera.rotation.y = 45/180*Math.PI;
-camera.position.x = 800;
-camera.position.y = 500;
-camera.position.z = 1000;
+let camera = new THREE.PerspectiveCamera( 10, window.innerWidth / window.innerHeight, 0.1, 10000 );
 
-hlight = new THREE.AmbientLight(0x303030);
+camera.rotation.y = 45/180*Math.PI;
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 1;
+
+hlight = new THREE.AmbientLight(0xd0d0d0);
 scene.add(hlight);
 
 let keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 1.0);
-keyLight.position.set(-100, 0, 100);
+keyLight.position.set(-10, 0, 10);
 
 let fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
-fillLight.position.set(100, 0, 100);
+fillLight.position.set(10, 0, 10);
 
 let backLight = new THREE.DirectionalLight(0xffffff, 1.0);
-backLight.position.set(100, 0, -100).normalize();
+backLight.position.set(10, 0, -10).normalize();
 
 scene.add(keyLight);
 scene.add(fillLight);
 scene.add(backLight);
 
-let mtlLoader = new THREE.MTLLoader();
-mtlLoader.load( mtlFile, function(materials) {
-  materials.preload();
+let loader = new THREE.GLTFLoader();
+loader.load('model/mouth.glb', function ( gltf ) {
 
-  let objLoader = new THREE.OBJLoader();
-  objLoader.setMaterials(materials);
-  objLoader.load( objFile, function (obj) {
-    scene.add(obj);
+  const box = new THREE.Box3().setFromObject( gltf.scene );
+  const center = box.getCenter( new THREE.Vector3() );
 
-    labels.map( label => {
+  gltf.scene.position.x += ( gltf.scene.position.x - center.x );
+  gltf.scene.position.y += ( gltf.scene.position.y - center.y );
+  gltf.scene.position.z += ( gltf.scene.position.z - center.z - 0.005 );
 
-      let { linkName, text, position } = label;
-      let { x, y, z } = position;
+  scene.add( gltf.scene );
 
-      let div = document.createElement('div');
-      div.className = 'label';
-      div.textContent = text;
-      div.style.marginTop = '-1em';
+  labels.map( label => {
 
-      div.addEventListener("click", routeToLink);
-      div.addEventListener("touchstart", routeToLink);
-      // div.addEventListener("click touchstart", function() {
-      //   location.href = 'https://ashley-koh.github.io/oral-cancer-screening/pages/' + linkName + '/index.html';
-      //   // location.href = 'oral-cancer-screening/pages/' + linkName + '/index.html';
-      // });
+    let { linkName, text, position } = label;
+    let { x, y, z } = position;
 
-      function routeToLink(event) {
-        event.preventDefault();
-        location.href = 'https://ashley-koh.github.io/oral-cancer-screening/pages/' + linkName + '/index.html';
-        // location.href = '/pages/' + linkName + '/index.html';
-      }
+    let div = document.createElement('div');
+    div.className = 'label';
+    div.textContent = text;
+    div.style.marginTop = '-1em';
 
-      label = new THREE.CSS2DObject( div );
-      label.position.set(x, y, z);
-      obj.add( label );
+    div.addEventListener("click", routeToLink);
+    div.addEventListener("touchstart", routeToLink);
 
-    })
+    function routeToLink(event) {
+      event.preventDefault();
+      location.href = 'https://ashley-koh.github.io/oral-cancer-screening/pages/' + linkName + '/index.html';
+      // location.href = '/pages/' + linkName + '/index.html';
+    }
 
-    animate();
-  },
-  function( xhr ){
-    console.log( (xhr.loaded / xhr.total * 100) + "% loaded")
-  }, 
-  function(error) {
-    console.log(error)
+    label = new THREE.CSS2DObject( div );
+    label.position.set(x, y, z);
+    gltf.scene.add( label );
+
   })
 
+  animate();
+
+},
+function ( xhr ) {
+  console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+},
+function ( error ) {
+  console.log(error)
 })
+
+// let mtlLoader = new THREE.MTLLoader();
+// mtlLoader.load( mtlFile, function(materials) {
+//   materials.preload();
+
+//   let objLoader = new THREE.OBJLoader();
+//   objLoader.setMaterials(materials);
+//   objLoader.load( objFile, function (obj) {
+//     scene.add(obj);
+
+    // labels.map( label => {
+
+    //   let { linkName, text, position } = label;
+    //   let { x, y, z } = position;
+
+    //   let div = document.createElement('div');
+    //   div.className = 'label';
+    //   div.textContent = text;
+    //   div.style.marginTop = '-1em';
+
+    //   div.addEventListener("click", routeToLink);
+    //   div.addEventListener("touchstart", routeToLink);
+
+    //   function routeToLink(event) {
+    //     event.preventDefault();
+    //     location.href = 'https://ashley-koh.github.io/oral-cancer-screening/pages/' + linkName + '/index.html';
+    //     // location.href = '/pages/' + linkName + '/index.html';
+    //   }
+
+    //   label = new THREE.CSS2DObject( div );
+    //   label.position.set(x, y, z);
+    //   obj.add( label );
+
+    // })
+
+//     animate();
+//   },
+//   function( xhr ){
+//     console.log( (xhr.loaded / xhr.total * 100) + "% loaded")
+//   }, 
+//   function(error) {
+//     console.log(error)
+//   })
+
+// })
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
