a4b53a4ba050177b4842a15c6d644d3d8baf856d
ashley-koh <36570313+ashley-koh@users.noreply.github.com>
2020-05-17 20:01:04 +0800

    added touch support and config.js file

--- pages/upper-mouth/index.js
+++ pages/upper-mouth/index.js
@@ -1,19 +1,28 @@
-const labels = [{
-    linkName: 'hard-palate',
-    text: 'Hard Palate',
-    position: { x: 0.1, y: 1.5, z: 0.2 }
-  },{
-    linkName: 'soft-palate',
-    text: 'Soft Palate',
-    position: { x: 0.1, y: -0.5, z: 0.2 }
-  }
+const { SOURCE } = config;
+
+const labels = [
+  {
+    linkName: "hard-palate",
+    text: "Hard Palate",
+    position: { x: 0.1, y: 1.5, z: 0.2 },
+  },
+  {
+    linkName: "soft-palate",
+    text: "Soft Palate",
+    position: { x: 0.1, y: -0.5, z: 0.2 },
+  },
 ];
 
 let scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xffffff );
+scene.background = new THREE.Color(0xffffff);
 
-let camera = new THREE.PerspectiveCamera( 10, window.innerWidth / window.innerHeight, 0.1, 10000 );
-camera.rotation.y = 45/180*Math.PI;
+let camera = new THREE.PerspectiveCamera(
+  10,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  10000
+);
+camera.rotation.y = (45 / 180) * Math.PI;
 camera.position.x = 0;
 camera.position.y = 0;
 camera.position.z = 50;
@@ -34,151 +43,176 @@ scene.add(keyLight);
 scene.add(fillLight);
 scene.add(backLight);
 
-
 let sprites = new THREE.Group();
 
 let loader = new THREE.GLTFLoader();
-loader.load('upper_mouth.glb', function ( gltf ) {
+loader.load(
+  "upper_mouth.glb",
+  function (gltf) {
+    const box = new THREE.Box3().setFromObject(gltf.scene);
+    const center = box.getCenter(new THREE.Vector3());
 
-const box = new THREE.Box3().setFromObject( gltf.scene );
-const center = box.getCenter( new THREE.Vector3() );
+    gltf.scene.position.x += gltf.scene.position.x - center.x;
+    gltf.scene.position.y += gltf.scene.position.y - center.y;
+    gltf.scene.position.z += gltf.scene.position.z - center.z - 0.005;
 
-gltf.scene.position.x += ( gltf.scene.position.x - center.x );
-gltf.scene.position.y += ( gltf.scene.position.y - center.y );
-gltf.scene.position.z += ( gltf.scene.position.z - center.z - 0.005 );
+    scene.add(gltf.scene);
 
-scene.add( gltf.scene );
+    gltf.scene.add(sprites);
 
-gltf.scene.add( sprites );
+    labels.map((label) => {
+      let { linkName, text, position } = label;
+      let { x, y, z } = position;
 
-labels.map( label => {
+      let spritey = makeTextSprite(text);
+      spritey.name = linkName;
+      spritey.position.set(x, y, z);
+      sprites.add(spritey);
+    });
 
-  let { linkName, text, position } = label;
-  let { x, y, z } = position;
-
-
-  let spritey = makeTextSprite( text );
-  spritey.name = linkName;
-  spritey.position.set(x, y, z);
-  sprites.add(spritey)
-
-})
-
-animate();
-
-},
-function ( xhr ) {
-console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-},
-function ( error ) {
-console.log(error)
-})
+    animate();
+  },
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  function (error) {
+    console.log(error);
+  }
+);
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setPixelRatio( window.devicePixelRatio );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
 
 let controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 function animate() {
+  // update the picking ray with the camera and mouse position
+  raycaster.setFromCamera(mouse, camera);
 
-// update the picking ray with the camera and mouse position
-raycaster.setFromCamera( mouse, camera );
+  // calculate objects intersecting the picking ray
+  let intersects = raycaster.intersectObjects(scene.children);
 
-// calculate objects intersecting the picking ray
-let intersects = raycaster.intersectObjects( scene.children );
+  for (let i = 0; i < intersects.length; i++) {
+    intersects[i].object.material.color.set(0xff0000);
+  }
 
-for ( let i = 0; i < intersects.length; i++ ) {
-  intersects[i].object.material.color.set( 0xff0000 );
+  renderer.render(scene, camera);
+  controls.update();
+  requestAnimationFrame(animate);
 }
 
-renderer.render( scene, camera );
-controls.update();
-requestAnimationFrame( animate );
-}
-
-window.addEventListener( 'resize', onWindowResize, false );
-window.addEventListener( 'mousemove', onMouseMove, false );
+window.addEventListener("resize", onWindowResize, false);
+window.addEventListener("mousemove", onMouseMove, false);
+window.addEventListener("touchstart", onTouchStart, false);
 
 function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-camera.aspect = window.innerWidth / window.innerHeight;
-camera.updateProjectionMatrix();
-
-renderer.setSize( window.innerWidth, window.innerHeight );
-
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 // Event Listeners to detect whether user input is click or drag
 let moved;
 let selectedObject = null;
 
-function onMouseMove( event ) {
+function onMouseMove(event) {
+  event.preventDefault();
 
-event.preventDefault();
+  moved = true;
 
-moved = true;
+  if (selectedObject) {
+    selectedObject.scale.set(1, 1, 1);
+    document.body.style.cursor = "default";
+    selectedObject = null;
+  }
 
-if ( selectedObject ) {
+  let intersects = getIntersects(event.layerX, event.layerY);
+  if (intersects.length > 0) {
+    let res = intersects.filter(function (res) {
+      return res && res.object;
+    })[0];
 
-  selectedObject.scale.set( 1, 1, 1 );
-  document.body.style.cursor = 'default';
-  selectedObject = null;
+    if (res && res.object) {
+      selectedObject = res.object;
+      selectedObject.scale.set(1.1, 1.1, 1.1);
+      document.body.style.cursor = "pointer";
 
-}
+      document.body.onmousedown = (e) => (moved = false);
+      document.body.onmouseup = (e) => route(e);
 
-let intersects = getIntersects( event.layerX, event.layerY );
-if ( intersects.length > 0 ) {
-
-  let res = intersects.filter( function ( res ) {
-    return res && res.object;
-  })[0];
-
-  if ( res && res.object ) {
-
-    selectedObject = res.object;
-    selectedObject.scale.set( 1.1, 1.1, 1.1 );
-    document.body.style.cursor = 'pointer';
-
-    document.body.onmousedown = (e) => moved = false;
-    document.body.ontouchstart = (e) => moved = false;
-    document.body.onmouseup = (e) => route(e);
-    document.body.ontouchend = (e) => route(e);
-
-    function route(e) {
-      if (!moved) {
-        // location.href = 'https://ashley-koh.github.io/oral-cancer-screening/pages/upper-mouth' + selectedObject.name + '/index.html';
-        location.href = '/pages/upper-mouth/' + selectedObject.name + '/index.html';
+      function route(e) {
+        if (!moved) {
+          if (config === "production") {
+            location.href =
+              "https://ashley-koh.github.io/oral-cancer-screening/pages/upper-mouth/" +
+              selectedObject.name +
+              "/index.html";
+          } else {
+            location.href =
+              "/pages/upper-mouth/" + selectedObject.name + "/index.html";
+          }
+        }
       }
     }
   }
-
 }
 
+function onTouchStart(event) {
+  const { clientX, clientY } = event.touches[0];
 
+  console.log({ clientX, clientY });
+
+  if (selectedObject) {
+    selectedObject.scale.set(1, 1, 1);
+    document.body.style.cursor = "default";
+    selectedObject = null;
+  }
+
+  let intersects = getIntersects(clientX, clientY);
+  if (intersects.length > 0) {
+    let res = intersects.filter(function (res) {
+      return res && res.object;
+    })[0];
+
+    if (res && res.object) {
+      selectedObject = res.object;
+      selectedObject.scale.set(1.1, 1.1, 1.1);
+
+      document.body.ontouchend = (e) => {
+        if (config === "production") {
+          location.href =
+            "https://ashley-koh.github.io/oral-cancer-screening/pages/upper-mouth/" +
+            selectedObject.name +
+            "/index.html";
+        } else {
+          location.href =
+            "/pages/upper-mouth/" + selectedObject.name + "/index.html";
+        }
+      };
+    }
+  }
 }
 
 let raycaster = new THREE.Raycaster();
 let mouse = new THREE.Vector2();
 
-function getIntersects( x, y ) {
-
+function getIntersects(x, y) {
   // calculate mouse position in normalized device coordinates
   // (-1 to +1) for both components
-  x = ( x / window.innerWidth ) * 2 - 1;
-  y = - ( y / window.innerHeight ) * 2 + 1;
+  x = (x / window.innerWidth) * 2 - 1;
+  y = -(y / window.innerHeight) * 2 + 1;
 
-  mouse.set( x, y );
-  raycaster.setFromCamera( mouse, camera );
+  mouse.set(x, y);
+  raycaster.setFromCamera(mouse, camera);
 
-  return raycaster.intersectObject( sprites, true );
-
+  return raycaster.intersectObject(sprites, true);
 }
 
 // Sprite Generator
-function makeTextSprite( message, linkName ) {
-
+function makeTextSprite(message, linkName) {
   let fontsize = 40;
   let borderThickness = 6;
   let canvas = document.createElement("canvas");
@@ -190,24 +224,25 @@ function makeTextSprite( message, linkName ) {
   let metric = context.measureText(message);
   let textWidth = metric.width;
 
-  console.log(message, textWidth)
+  console.log(message, textWidth);
 
   context.font = "Bold " + fontsize + "px Arial";
   // context.fillText(message, borderThickness, fontsize + borderThickness);
   context.fillText(message, borderThickness, fontsize + borderThickness);
 
-  var texture = new THREE.Texture(canvas)
-  texture.magFilter=THREE.NearestFilter;
+  var texture = new THREE.Texture(canvas);
+  texture.magFilter = THREE.NearestFilter;
   texture.needsUpdate = true;
 
-  var spriteMaterial = new THREE.SpriteMaterial( { map: texture, color: 0xffffff } );
-  var sprite = new THREE.Sprite( spriteMaterial );
-  return sprite;  
+  var spriteMaterial = new THREE.SpriteMaterial({
+    map: texture,
+    color: 0xffffff,
+  });
+  var sprite = new THREE.Sprite(spriteMaterial);
+  return sprite;
 }
 
-function route( partName ) {
-
-// location.href =  location.href = 'https://ashley-koh.github.io/oral-cancer-screening/pages/upper-mouth/' + partName + '/index.html';
-location.href = '/pages/upper-mouth/' + partName + '/index.html';
-
+function route(partName) {
+  // location.href =  location.href = 'https://ashley-koh.github.io/oral-cancer-screening/pages/upper-mouth/' + partName + '/index.html';
+  location.href = "/pages/upper-mouth/" + partName + "/index.html";
 }
